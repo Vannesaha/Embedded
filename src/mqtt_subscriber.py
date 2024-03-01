b631a18b9c0412da13075fc41d7aa27b11752a84
asku1990 <a-p.kvs@pm.me>
2024-03-01 17:43:19 +0200

    hydraulic ui ask for cylinder 0-3 and position 0-180

--- src/mqtt_subscriber.py
+++ src/mqtt_subscriber.py
@@ -1,6 +1,4 @@
 import paho.mqtt.client as mqtt
-from src.hydraulics.cylinders import set_position
-
 
 from config.settings import (
     BROKER,  # MQTT broker address
@@ -9,15 +7,13 @@ from config.settings import (
     DIRECT_TOPIC,  # MQTT topic for direct messages
     LWT_MESSAGE,  # Last Will and Testament message
     OFFLINE_MESSAGE,  # Offline message
-    MQTT_CLIENT_ID,  # Unique client identifier
     DEVICE_ID,  # Unique device identifier
-    RESPONSE_TOPIC,  # MQTT topic for responses
 )
 
 
 class MQTTSubscriber:
     def __init__(self):
-        self.client = mqtt.Client(client_id=MQTT_CLIENT_ID)
+        self.client = mqtt.Client(client_id=DEVICE_ID)
         self.client.will_set(STATUS_TOPIC, payload=LWT_MESSAGE, qos=1, retain=True)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
